5ec26ab774a5de16a77a091a73fe72955bfde07e
Sitinurhayati10 <90059192+Sitinurhayati10@users.noreply.github.com>
2024-06-08 09:10:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -94,17 +94,17 @@ if st.button("Prediksi"):
         st.info("Sedang melakukan prediksi...")
 
         # Load model SVM dan vectorizer
-        with open("C:\\Skilvul Grup 22 Education\\bookgenie-genreprediction\\svm_model.pkl", 'rb') as file:
+        with open("./svm_model.pkl", 'rb') as file:
             loaded_model = pickle.load(file)
         
-        with open("C:\\Skilvul Grup 22 Education\\bookgenie-genreprediction\\tfidf_vectorizer.pkl", 'rb') as file:
+        with open("./tfidf_vectorizer.pkl", 'rb') as file:
             tfidf = pickle.load(file)
         
         # Preprocessing deskripsi buku
         book_description_processed = [stem_text(remove_sw(removepunc(lowercase(book_description))))]
 
         # Membaca data X_train
-        X_train = pd.read_csv("C:\\Skilvul Grup 22 Education\\bookgenie-genreprediction\\X_train_tfidf.csv")  # Ubah sesuai dengan lokasi yang benar
+        X_train = pd.read_csv("./X_train_tfidf.csv")  # Ubah sesuai dengan lokasi yang benar
         
         # Menerapkan pemrosesan teks pada data X_train
         X_train['Combined_Text'] = X_train['Combined_Text'].apply(lowercase)
